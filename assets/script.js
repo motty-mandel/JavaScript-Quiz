e17d5894d8e49ba01a0195562e041d5ad597c694
motty mandel <mandelmottyisrael65@gmail.com>
2023-07-26 23:24:38 -0400

    finishing my work. Ihave no idea why the next buttons move to the left

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
+// Finding references to all the buttons and questions and answers from the html
 let startQuizBtn = document.querySelector('#startQuizBtn');
 let timeLeft = document.querySelector('#timeLeft')
 let viewHighScore = document.querySelector('#viewHighScore')
 let header = document.querySelector('#header');
 let quiz = document.querySelector('#quiz');
+let input = document.querySelector('#input');
+let submitScore = document.querySelector('#submitScore');
 let nextBtn1 = document.querySelector('#nextBtn1');
 let nextBtn2 = document.querySelector('#nextBtn2');
 let nextBtn3 = document.querySelector('#nextBtn3');
@@ -11,39 +14,48 @@ let answerA = document.querySelector('#answerA');
 let answerB = document.querySelector('#answerB');
 let answerC = document.querySelector('#answerC');
 let answerD = document.querySelector('#answerD');
-
 let addingYourInfo = document.querySelector('#addingYourInfo');
+
 let ansChoice = "";
 let score = 0
 let iterationNumber = 0;
+let scoreAmount = localStorage.getItem('score');
 
+// Making a list questions to ask
 let questionList = [
     "When was the first iPhone released?",
     "In what year did the space shuttle Columbia expload?",
     "What was the year before 2000?"
 ];
 
-// let aAnswerList = ["2007", "12MP", "iPhone 8"];
-// let bAnswerList = ["2013", "50MP", "iPhone X"];
-// let cAnswerList = ["2003", "100MP", "iPhone 12"];
-// let dAnswerList = ["1999", "3,200", "Phone 14"];
-
+// Hiding the questions until needed
 quiz.style.display = "none";
-addingYourInfo.style.display = "none"
+addingYourInfo.style.display = "none";
 
+// I tried getting this to work
+// submitScore.addEventListener('click', function() {
+//     var initials = input;
+
+//     localStorage.setItem("score", score);
+
+//     localStorage.setItem("initials", initials);
+// });
+
+// Starting the timer, hiding the header and most of the buttons and showing the questions
 function startQuiz() {
     setTime()
     header.innerHTML = "";
+    viewHighScore.style.display = "none"
     startQuizBtn.style.display = "none";
-    viewHighScore.style.display = "none";
-    nextBtn2.style.display = "none"
-    nextBtn3.style.display = "none"
+    nextBtn2.style.display = "none";
+    nextBtn3.style.display = "none";
     quiz.style.display = "block";
     question.innerHTML = questionList[iterationNumber];
 }
 
 var secondsLeft = 20;
 
+// Creating the timer
 function setTime() {
     // Sets interval in variable
    var timerInterval = setInterval(function() {
@@ -57,12 +69,12 @@ function setTime() {
        quiz.style.display = "none";
        addingYourInfo.style.display = "block";
        timeLeft.innerHTML= "";
-
      }
 
    }, 1000);
  }
 
+ // Making the chosen answer red and the others white
 function aFunction() {
     answerA.style.color = "red";
     answerB.style.color = "white";
@@ -95,6 +107,7 @@ function dFunction() {
     ansChoice = "d";
 }
 
+// Checking to see if the answers are right
 function nextQn1() {
     if (ansChoice == "a") {
         score += 1;
@@ -125,4 +138,3 @@ function nextQn3() {
     } 
     secondsLeft = 0;
 }
-    
